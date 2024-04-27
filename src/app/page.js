17db9c5b87f61d7f26bc44b34110d8143d9b96b0
CyberSleeper <mahartha.gemilang@gmail.com>
2024-04-27 17:41:07 +0700

    add(auth): register page

--- src/app/page.js
+++ src/app/page.js
@@ -10,10 +10,10 @@ export default function Home() {
       <h1 className="text-3xl font-semibold">
         Unlimited movies, TV shows, and more
       </h1>
-      <Link href="/login" className="bg-red-primary w-28 justify-center flex rounded-lg py-1.5 font-semibold">
+      <Link href="/login" className="hover:scale-105 active:scale-95 active:opacity-70 transition-all bg-red-primary w-28 justify-center flex rounded-lg py-1.5 font-semibold">
         Login
       </Link>
-      <Link href="/register" className="bg-red-primary w-28 justify-center flex rounded-lg py-1.5 font-semibold">
+      <Link href="/register" className="hover:scale-105 active:scale-95 active:opacity-70 transition-all bg-red-primary w-28 justify-center flex rounded-lg py-1.5 font-semibold">
         Register
       </Link>
     </main>
