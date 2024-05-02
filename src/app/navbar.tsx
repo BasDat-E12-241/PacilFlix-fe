6e5b69421c7e754027dc1fa904f471294c2b3ea7
CyberSleeper <mahartha.gemilang@gmail.com>
2024-05-02 10:23:17 +0700

    add(navbar): Add not logged in state

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -16,31 +16,49 @@ function NavLink({ href, isActive, children }) {
 export default function Navbar() {
   const pathname = usePathname();
 
+  // Change this to get different navbar
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
   return (
-    <nav className="flex gap-16 px-8 py-4 items-center justify-between bg-primary fixed w-full left-0">
+    <nav className="flex gap-16 px-8 py-4 items-center bg-primary fixed w-full left-0">
       <a className="font-semibold text-2xl text-red-primary" href="/">PacilFlix</a>
-      <div>
-        <ul className="flex gap-4">
-          <NavLink href="/daftar-tayangan" isActive={pathname === "/daftar-tayangan"}>
-            Daftar Tayangan
-          </NavLink>
-          <NavLink href="/daftar-kontributor" isActive={pathname === "/daftar-kontributor"}>
-            Daftar Kontributor
-          </NavLink>
-          <NavLink href="/daftar-favorit" isActive={pathname === "/daftar-favorit"}>
-            Daftar Favorit
-          </NavLink>
-          <NavLink href="/daftar-unduhan" isActive={pathname === "/daftar-unduhan"}>
-            Daftar Unduhan
-          </NavLink>
-          <NavLink href="/langganan" isActive={pathname === "/langganan"}>
-            Langganan
-          </NavLink>
-        </ul>
-      </div>
-      <div className="border-2 border-red-primary px-4 rounded-full">
-        Logout
-      </div>
-    </nav>
+      {isLoggedIn ? 
+        <div className="justify-between flex w-full">
+          <div>
+            <ul className="flex gap-4">
+              <NavLink href="/trailer" isActive={pathname === "/trailer"}>
+                Trailer
+              </NavLink>
+              
+            </ul>
+          </div>
+        </div>
+      :
+        <div className="justify-between flex w-full">
+          <div>
+            <ul className="flex gap-4">
+              <NavLink href="/daftar-tayangan" isActive={pathname === "/daftar-tayangan"}>
+                Daftar Tayangan
+              </NavLink>
+              <NavLink href="/daftar-kontributor" isActive={pathname === "/daftar-kontributor"}>
+                Daftar Kontributor
+              </NavLink>
+              <NavLink href="/daftar-favorit" isActive={pathname === "/daftar-favorit"}>
+                Daftar Favorit
+              </NavLink>
+              <NavLink href="/daftar-unduhan" isActive={pathname === "/daftar-unduhan"}>
+                Daftar Unduhan
+              </NavLink>
+              <NavLink href="/langganan" isActive={pathname === "/langganan"}>
+                Langganan
+              </NavLink>
+            </ul>
+          </div>
+          <div className="border-2 border-red-primary px-4 rounded-full">
+            Logout
+          </div>
+        </div>
+      }
+      </nav>
   );
 }
