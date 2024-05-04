7c4972a7a5ea6d9c9b41a36b56057f2f9a9faab2
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-05-04 15:52:18 +0700

    style(trailer, tayangan): deleted unnecessary lines

--- src/app/trailer/page.tsx
+++ src/app/trailer/page.tsx
@@ -78,137 +78,135 @@ const seriesTrailers = [
   },
   // Tambahkan trailer series lain di sini
 ];
-const [isActive, setIsActive] = useState(false);
-const [country, setCountry] = useState("Indonesia");
-
-const handleGlobalButtonClick = () => {
-  setIsActive(true);
-};
-
-const handleCountryButtonClick = () => {
-  setIsActive(false);
-};
 
 export default function Trailer() {
-  <section className="bg-primary min-h-screen flex flex-col items-center justify-center gap-8">
-    <h1 className="text-2xl font-semibold">Trailer</h1>
-    <form className="flex flex-col items-center gap-6">
-      <label className="flex flex-col gap-2">
-        <input
-          type="text"
-          placeholder="Search"
-          required
-          className="border-4 transition-all border-solid rounded-lg px-3 py-1.5 w-164 bg-white text-black focus:border-red-primary"
-        />
-      </label>
-    </form>
-    <div className="max-w-3xl w-full p-4 rounded-lg shadow-md">
-      <section>
-        <h2 className="text-lg font-bold my-4">10 Tayangan Terbaik Minggu Ini</h2>
-        <div className="flex mt-4">
-          <div
-            className={`rounded-full border-2 border-red-500 mr-4 flex justify-center items-center p-1 w-56 ${isActive ? "bg-red-500" : "border-red-500"}`}
-            onClick={handleGlobalButtonClick}
-          >
-            <span className="text-white text-base">Opsi Top 10 Global</span>
+  const [isActive, setIsActive] = useState(false);
+  const [country, setCountry] = useState("Indonesia");
+  
+  const handleGlobalButtonClick = () => {
+    setIsActive(true);
+  };
+  
+  const handleCountryButtonClick = () => {
+    setIsActive(false);
+  };
+  return (
+
+    <section className="bg-primary min-h-screen flex flex-col items-center justify-center gap-8 mt-16">
+      <h1 className="text-2xl font-semibold">Trailer</h1>
+      <form className="flex flex-col items-center gap-6">
+        <label className="flex flex-col gap-2">
+          <input
+            type="text"
+            placeholder="Search"
+            required
+            className="border-4 transition-all border-solid rounded-lg px-3 py-1.5 w-164 bg-white text-black focus:border-red-primary"
+          />
+        </label>
+      </form>
+      <div className="max-w-3xl w-full p-4 rounded-lg shadow-md">
+        <section>
+          <h2 className="text-lg font-bold my-4">10 Tayangan Terbaik Minggu Ini</h2>
+          <div className="flex mt-4">
+            <div
+              className={`rounded-full border-2 border-red-primary mr-4 flex justify-center items-center p-1 w-56 ${isActive ? "bg-red-primary" : "border-red-primary"}`}
+              onClick={handleGlobalButtonClick}
+            >
+              <span className="text-white text-base">Opsi Top 10 Global</span>
+            </div>
+            <div
+              className={`rounded-full border-2 border-red-primary flex justify-center items-center p-1 w-56 ${isActive ? "border-red-primary" : "bg-red-primary"}`}
+              onClick={handleCountryButtonClick}
+            >
+              <span className="text-white text-base">Opsi Top 10 {country}</span>
+            </div>
           </div>
-          <div
-            className={`rounded-full border-2 border-red-500 flex justify-center items-center p-1 w-56 ${isActive ? "border-red-500" : "bg-red-500"}`}
-            onClick={handleCountryButtonClick}
-          >
-            <span className="text-white text-base">Opsi Top 10 {country}</span>
-          </div>
-        </div>
-        <table className="w-full my-4 text-left border-collapse border border-gray-400">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 px-4 py-2">Peringkat</th>
-              <th className="border border-gray-300 px-4 py-2">Judul</th>
-              <th className="border border-gray-300 px-4 py-2">Sinopsis Trailer</th>
-              <th className="border border-gray-300 px-4 py-2">URL Trailer</th>
-              <th className="border border-gray-300 px-4 py-2">Tanggal Rilis Trailer</th>
-              <th className="border border-gray-300 px-4 py-2">Total view 7 hari terakhir</th>
-            </tr>
-          </thead>
-          <tbody>
-            {isActive
-              ? trailers_global.map((trailer, index) => (
+          <table className="w-full my-4 text-left border-collapse border border-gray-400">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 px-4 py-2">Peringkat</th>
+                <th className="border border-gray-300 px-4 py-2">Judul</th>
+                <th className="border border-gray-300 px-4 py-2">Sinopsis Trailer</th>
+                <th className="border border-gray-300 px-4 py-2">URL Trailer</th>
+                <th className="border border-gray-300 px-4 py-2">Tanggal Rilis Trailer</th>
+                <th className="border border-gray-300 px-4 py-2">Total view 7 hari terakhir</th>
+              </tr>
+            </thead>
+            <tbody>
+              {isActive
+                ? trailers_global.map((trailer, index) => (
+                  <tr key={index}>
+                    <td className="border border-gray-300 text-center px-4 py-2">{trailer.peringkat}</td>
+                    <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
+                    <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
+                    <td className="border border-gray-300 px-4 py-2"><a href={trailer.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
+                    <td className="border border-gray-300 px-4 py-2">{trailer.tanggalRilis}</td>
+                    <td className="border border-gray-300 text-center px-4 py-2">{trailer.totalView}</td>
+                  </tr>
+                ))
+                : trailers_country.map((trailer, index) => (
+                  <tr key={index}>
+                    <td className="border border-gray-300 text-center px-4 py-2">{trailer.peringkat}</td>
+                    <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
+                    <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
+                    <td className="border border-gray-300 px-4 py-2"><a href={trailer.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
+                    <td className="border border-gray-300 px-4 py-2">{trailer.tanggalRilis}</td>
+                    <td className="border border-gray-300 text-center px-4 py-2">{trailer.totalView}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </section>
+  
+        <section>
+          <h2 className="text-lg font-bold my-4">Film</h2>
+          <table className="w-full my-4 text-left border-collapse border border-gray-400">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 px-4 py-2">Judul</th>
+                <th className="border border-gray-300 px-4 py-2">Sinopsis Trailer</th>
+                <th className="border border-gray-300 px-4 py-2">URL Trailer</th>
+                <th className="border border-gray-300 px-4 py-2">Tanggal Rilis Trailer</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filmTrailers.map((trailer, index) => (
                 <tr key={index}>
-                  <td className="border border-gray-300 text-center px-4 py-2">{trailer.peringkat}</td>
                   <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
                   <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
                   <td className="border border-gray-300 px-4 py-2"><a href={trailer.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
                   <td className="border border-gray-300 px-4 py-2">{trailer.tanggalRilis}</td>
-                  <td className="border border-gray-300 text-center px-4 py-2">{trailer.totalView}</td>
                 </tr>
-              ))
-              : trailers_country.map((trailer, index) => (
+              ))}
+            </tbody>
+          </table>
+        </section>
+  
+        <section>
+          <h2 className="text-lg font-bold my-4">Series</h2>
+          <table className="w-full my-4 text-left border-collapse border border-gray-400">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 px-4 py-2">Judul</th>
+                <th className="border border-gray-300 px-4 py-2">Sinopsis Trailer</th>
+                <th className="border border-gray-300 px-4 py-2">URL Trailer</th>
+                <th className="border border-gray-300 px-4 py-2">Tanggal Rilis Trailer</th>
+              </tr>
+            </thead>
+            <tbody>
+              {seriesTrailers.map((trailer, index) => (
                 <tr key={index}>
-                  <td className="border border-gray-300 text-center px-4 py-2">{trailer.peringkat}</td>
                   <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
                   <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
                   <td className="border border-gray-300 px-4 py-2"><a href={trailer.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
                   <td className="border border-gray-300 px-4 py-2">{trailer.tanggalRilis}</td>
-                  <td className="border border-gray-300 text-center px-4 py-2">{trailer.totalView}</td>
                 </tr>
-              ))
-            }
-          </tbody>
-        </table>
-      </section>
-
-      <section>
-        <h2 className="text-lg font-bold my-4">Film</h2>
-        <table className="w-full my-4 text-left border-collapse border border-gray-400">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 px-4 py-2">Peringkat</th>
-              <th className="border border-gray-300 px-4 py-2">Judul</th>
-              <th className="border border-gray-300 px-4 py-2">Sinopsis Trailer</th>
-              <th className="border border-gray-300 px-4 py-2">URL Trailer</th>
-              <th className="border border-gray-300 px-4 py-2">Tanggal Rilis Trailer</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filmTrailers.map((trailer, index) => (
-              <tr key={index}>
-                <td className="border border-gray-300 text-center px-4 py-2">{trailer.peringkat}</td>
-                <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
-                <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
-                <td className="border border-gray-300 px-4 py-2"><a href={trailer.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
-                <td className="border border-gray-300 px-4 py-2">{trailer.tanggalRilis}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
-
-      <section>
-        <h2 className="text-lg font-bold my-4">Series</h2>
-        <table className="w-full my-4 text-left border-collapse border border-gray-400">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 px-4 py-2">Peringkat</th>
-              <th className="border border-gray-300 px-4 py-2">Judul</th>
-              <th className="border border-gray-300 px-4 py-2">Sinopsis Trailer</th>
-              <th className="border border-gray-300 px-4 py-2">URL Trailer</th>
-              <th className="border border-gray-300 px-4 py-2">Tanggal Rilis Trailer</th>
-            </tr>
-          </thead>
-          <tbody>
-            {seriesTrailers.map((trailer, index) => (
-              <tr key={index}>
-                <td className="border border-gray-300 text-center px-4 py-2">{trailer.peringkat}</td>
-                <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
-                <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
-                <td className="border border-gray-300 px-4 py-2"><a href={trailer.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
-                <td className="border border-gray-300 px-4 py-2">{trailer.tanggalRilis}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
-    </div>
-  </section>
-    ;
+              ))}
+            </tbody>
+          </table>
+        </section>
+      </div>
+    </section>
+  );
 }
