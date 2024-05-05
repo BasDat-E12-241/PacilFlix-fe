456eed8350df72c7d86f594501067ca2486136c0
CyberSleeper <mahartha.gemilang@gmail.com>
2024-05-05 23:37:29 +0700

    add(login): form fetch

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,34 @@
+"use client"
+
 import React from "react";
+import { useState } from "react";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 
+        username, 
+        password 
+      }),
+    })
+    .then(response => response.json())
+    .then(data => alert(data.message))
+    .catch(error => alert(error.message));
+  }
+
   return (
     <section className="bg-primary min-h-screen flex flex-col items-center justify-center gap-8">
       <h1 className="text-2xl font-semibold">Form Login</h1>
-      <form className="flex flex-col items-center gap-6">
+      <form className="flex flex-col items-center gap-6" onSubmit={handleSubmit}>
         <label className="flex flex-col gap-2">
           <span className="font-semibold">Username</span>
           <input 
@@ -12,6 +36,7 @@ export default function Register() {
             placeholder="Username"
             required
             className="border-4 transition-all border-solid rounded-lg px-3 py-1.5 w-64 bg-white text-black focus:border-red-primary"
+            onChange={(event) => setUsername(event.target.value)}
           />
         </label>
         <label className="flex flex-col gap-2">
@@ -21,6 +46,7 @@ export default function Register() {
             placeholder="Password"
             required
             className="border-4 transition-all border-solid rounded-lg px-3 py-1.5 w-64 bg-white text-black focus:border-red-primary"
+            onChange={(event) => setPassword(event.target.value)}
           />
         </label>
         <button type="submit" className="hover:scale-105 active:scale-95 active:opacity-70 transition-all bg-red-primary w-28 justify-center flex rounded-lg py-1.5 font-semibold">
