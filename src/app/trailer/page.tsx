9d0c513622d9a219b23e61a8081029f72170b888
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-05-17 06:33:35 +0700

    feat(trailer): implement functionality without search

--- src/app/trailer/page.tsx
+++ src/app/trailer/page.tsx
@@ -1,124 +1,151 @@
 'use client'
 
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 
-// Dummy data for the trailer
-const trailers_global = [
-  {
-    peringkat: 1,
-    judul: "Judul Trailer 1 Global",
-    sinopsis: "Sinopsis Trailer 1",
-    url: "https://www.youtube.com/watch?v=video1",
-    tanggalRilis: "01/01/2022",
-    totalView: 10000,
-  },
-  {
-    peringkat: 2,
-    judul: "Judul Trailer 2 Global",
-    sinopsis: "Sinopsis Trailer 2 Global",
-    url: "https://www.youtube.com/watch?v=video2",
-    tanggalRilis: "02/01/2022",
-    totalView: 15000,
-  },
-  // Tambahkan trailer lain di sini
-];
+type Trailer = {
+  peringkat: number;
+  judul: string;
+  sinopsis: string;
+  url_video_trailer: string;
+  release_date_trailer: string;
+  total_view: number;
+};
 
-const trailers_country = [
-  {
-    peringkat: 1,
-    judul: "Judul Trailer 1 Country",
-    sinopsis: "Sinopsis Trailer 1 Country",
-    url: "https://www.youtube.com/watch?v=video1",
-    tanggalRilis: "01/01/2022",
-    totalView: 10000,
-  },
-  {
-    peringkat: 2,
-    judul: "Judul Trailer 2 Country",
-    sinopsis: "Sinopsis Trailer 2 Country",
-    url: "https://www.youtube.com/watch?v=video2",
-    tanggalRilis: "02/01/2022",
-    totalView: 15000,
-  },
-  // Tambahkan trailer lain di sini
-];
+type FilmTrailer = {
+  judul: string;
+  sinopsis: string;
+  url_video_trailer: string;
+  release_date_trailer: string;
+};
 
-const filmTrailers = [
-  {
-    peringkat: 1,
-    judul: "Film Trailer 1",
-    sinopsis: "Sinopsis Film Trailer 1",
-    url: "https://www.youtube.com/watch?v=film1",
-    tanggalRilis: "01/01/2022",
-  },
-  {
-    peringkat: 2,
-    judul: "Film Trailer 2",
-    sinopsis: "Sinopsis Film Trailer 2",
-    url: "https://www.youtube.com/watch?v=film2",
-    tanggalRilis: "02/01/2022",
-  },
-  // Tambahkan trailer film lain di sini
-];
+type SeriesTrailer = {
+  judul: string;
+  sinopsis: string;
+  url_video_trailer: string;
+  release_date_trailer: string;
+};
 
-const seriesTrailers = [
-  {
-    peringkat: 1,
-    judul: "Series Trailer 1",
-    sinopsis: "Sinopsis Series Trailer 1",
-    url: "https://www.youtube.com/watch?v=series1",
-    tanggalRilis: "01/01/2022",
-  },
-  {
-    peringkat: 2,
-    judul: "Series Trailer 2",
-    sinopsis: "Sinopsis Series Trailer 2",
-    url: "https://www.youtube.com/watch?v=series2",
-    tanggalRilis: "02/01/2022",
-  },
-  // Tambahkan trailer series lain di sini
-];
+const fetchGlobalsTrailers = async (search: string = "") => {
+  try {
+    const response = await fetch(`/api/trailer/global?search=${search}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Failed to fetch global trailers:', error);
+    return [];
+  }
+};
+
+const fetchFilmTrailers = async () => {
+  try {
+    const response = await fetch(`/api/trailer/film`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Failed to fetch film trailers:', error);
+    return [];
+  }
+};
+
+const fetchSeriesTrailers = async () => {
+  try {
+    const response = await fetch(`/api/trailer/series`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Failed to fetch series trailers:', error);
+    return [];
+  }
+};
 
 export default function Trailer() {
-  const [isActive, setIsActive] = useState(true);
-  const [country, setCountry] = useState("Indonesia");
-  
-  const handleGlobalButtonClick = () => {
-    setIsActive(true);
-  };
-  
-  const handleCountryButtonClick = () => {
-    setIsActive(false);
-  };
-  return (
+  const [trailersGlobal, setTrailersGlobal] = useState<Trailer[]>([]);
+  const [filmTrailers, setFilmTrailers] = useState<FilmTrailer[]>([]);
+  const [seriesTrailers, setSeriesTrailers] = useState<SeriesTrailer[]>([]);
+  const [search, setSearchValue] = useState<string>("");
+  const [searchResults, setSearchResults] = useState<Trailer[]>([]);
 
+  
+  useEffect(() => {
+    const loadGlobalsTrailers = async () => {
+      const data = await fetchGlobalsTrailers();
+      setTrailersGlobal(data);
+    };
+
+    loadGlobalsTrailers();
+  }, []);
+
+
+  useEffect(() => {
+    if (search) {
+      const searchTrailers = async () => {
+        const data = await fetchGlobalsTrailers(search);
+        setSearchResults(data);
+      };
+
+      searchTrailers();
+    } else {
+      setSearchResults([]);
+    }
+  }, [search]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+    }
+  };
+
+  useEffect(() => {
+    const loadFilmTrailers = async () => {
+      const data = await fetchFilmTrailers();
+      setFilmTrailers(data);
+    };
+
+    loadFilmTrailers();
+  }, []);
+
+  useEffect(() => {
+    const loadSeriesTrailers = async () => {
+      const data = await fetchSeriesTrailers();
+      setSeriesTrailers(data);
+    };
+
+    loadSeriesTrailers();
+  }, []);
+
+  function getTrailersToDisplay(): Trailer[] {
+    return search ? searchResults : trailersGlobal;
+  }
+
+  const trailersToDisplay: Trailer[] = getTrailersToDisplay();
+
+  return (
     <section className="bg-primary min-h-screen flex flex-col items-center justify-center gap-8 mt-16">
       <h1 className="text-2xl font-semibold">Trailer</h1>
       <form className="flex flex-col items-center gap-6">
         <label className="flex flex-col gap-2">
           <input
             type="text"
+            value={search}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
             placeholder="Search"
-            required
             className="border-4 transition-all border-solid rounded-lg px-3 py-1.5 w-164 bg-white text-black focus:border-red-primary"
           />
         </label>
       </form>
-      <div className="max-w-3xl w-full p-4 rounded-lg shadow-md">
+      <div className="max-w-3xl w-full p-4 rounded-lg shadow-md mx-auto flex flex-col items-center">
         <section>
           <h2 className="text-lg font-bold my-4">10 Tayangan Terbaik Minggu Ini</h2>
           <div className="flex mt-4">
             <div
-              className={`rounded-full border-2 border-red-primary mr-4 flex justify-center items-center p-1 w-56 ${isActive ? "bg-red-primary" : "border-red-primary"}`}
-              onClick={handleGlobalButtonClick}
+              className={`rounded-full bg-red-primary mr-4 flex justify-center items-center p-1 w-56`}
             >
               <span className="text-white text-base">Opsi Top 10 Global</span>
-            </div>
-            <div
-              className={`rounded-full border-2 border-red-primary flex justify-center items-center p-1 w-56 ${isActive ? "border-red-primary" : "bg-red-primary"}`}
-              onClick={handleCountryButtonClick}
-            >
-              <span className="text-white text-base">Opsi Top 10 {country}</span>
             </div>
           </div>
           <table className="w-full my-4 text-left border-collapse border border-gray-400">
@@ -133,79 +160,72 @@ export default function Trailer() {
               </tr>
             </thead>
             <tbody>
-              {isActive
-                ? trailers_global.map((trailer, index) => (
+              {trailersToDisplay?.map((trailer, index) => (
                   <tr key={index}>
                     <td className="border border-gray-300 text-center px-4 py-2">{trailer.peringkat}</td>
                     <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
                     <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
-                    <td className="border border-gray-300 px-4 py-2"><a href={trailer.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
-                    <td className="border border-gray-300 px-4 py-2">{trailer.tanggalRilis}</td>
-                    <td className="border border-gray-300 text-center px-4 py-2">{trailer.totalView}</td>
-                  </tr>
-                ))
-                : trailers_country.map((trailer, index) => (
-                  <tr key={index}>
-                    <td className="border border-gray-300 text-center px-4 py-2">{trailer.peringkat}</td>
-                    <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
-                    <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
-                    <td className="border border-gray-300 px-4 py-2"><a href={trailer.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
-                    <td className="border border-gray-300 px-4 py-2">{trailer.tanggalRilis}</td>
-                    <td className="border border-gray-300 text-center px-4 py-2">{trailer.totalView}</td>
+                    <td className="border border-gray-300 px-4 py-2"><a href={trailer.url_video_trailer} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
+                    <td className="border border-gray-300 px-4 py-2">{trailer.release_date_trailer}</td>
+                    <td className="border border-gray-300 text-center px-4 py-2">{trailer.total_view}</td>
                   </tr>
                 ))
               }
             </tbody>
           </table>
         </section>
-  
-        <section>
-          <h2 className="text-lg font-bold my-4">Film</h2>
-          <table className="w-full my-4 text-left border-collapse border border-gray-400">
-            <thead>
-              <tr>
-                <th className="border border-gray-300 px-4 py-2">Judul</th>
-                <th className="border border-gray-300 px-4 py-2">Sinopsis Trailer</th>
-                <th className="border border-gray-300 px-4 py-2">URL Trailer</th>
-                <th className="border border-gray-300 px-4 py-2">Tanggal Rilis Trailer</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filmTrailers.map((trailer, index) => (
-                <tr key={index}>
-                  <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
-                  <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
-                  <td className="border border-gray-300 px-4 py-2"><a href={trailer.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
-                  <td className="border border-gray-300 px-4 py-2">{trailer.tanggalRilis}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </section>
-  
-        <section>
-          <h2 className="text-lg font-bold my-4">Series</h2>
-          <table className="w-full my-4 text-left border-collapse border border-gray-400">
-            <thead>
-              <tr>
-                <th className="border border-gray-300 px-4 py-2">Judul</th>
-                <th className="border border-gray-300 px-4 py-2">Sinopsis Trailer</th>
-                <th className="border border-gray-300 px-4 py-2">URL Trailer</th>
-                <th className="border border-gray-300 px-4 py-2">Tanggal Rilis Trailer</th>
-              </tr>
-            </thead>
-            <tbody>
-              {seriesTrailers.map((trailer, index) => (
-                <tr key={index}>
-                  <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
-                  <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
-                  <td className="border border-gray-300 px-4 py-2"><a href={trailer.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
-                  <td className="border border-gray-300 px-4 py-2">{trailer.tanggalRilis}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </section>
+
+        {!search && (
+          <>
+            <section>
+              <h2 className="text-lg font-bold my-4">Film</h2>
+              <table className="w-full my-4 text-left border-collapse border border-gray-400">
+                <thead>
+                  <tr>
+                    <th className="border border-gray-300 px-4 py-2">Judul</th>
+                    <th className="border border-gray-300 px-4 py-2">Sinopsis Trailer</th>
+                    <th className="border border-gray-300 px-4 py-2">URL Trailer</th>
+                    <th className="border border-gray-300 px-4 py-2">Tanggal Rilis Trailer</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {filmTrailers?.map((trailer, index) => (
+                    <tr key={index}>
+                      <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
+                      <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
+                      <td className="border border-gray-300 px-4 py-2"><a href={trailer.url_video_trailer} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
+                      <td className="border border-gray-300 px-4 py-2">{trailer.release_date_trailer}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </section>
+
+            <section>
+              <h2 className="text-lg font-bold my-4">Series</h2>
+              <table className="w-full my-4 text-left border-collapse border border-gray-400">
+                <thead>
+                  <tr>
+                    <th className="border border-gray-300 px-4 py-2">Judul</th>
+                    <th className="border border-gray-300 px-4 py-2">Sinopsis Trailer</th>
+                    <th className="border border-gray-300 px-4 py-2">URL Trailer</th>
+                    <th className="border border-gray-300 px-4 py-2">Tanggal Rilis Trailer</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {seriesTrailers?.map((trailer, index) => (
+                    <tr key={index}>
+                      <td className="border border-gray-300 px-4 py-2">{trailer.judul}</td>
+                      <td className="border border-gray-300 px-4 py-2">{trailer.sinopsis}</td>
+                      <td className="border border-gray-300 px-4 py-2"><a href={trailer.url_video_trailer} className="text-blue-500" target="_blank" rel="noopener noreferrer">Lihat Trailer</a></td>
+                      <td className="border border-gray-300 px-4 py-2">{trailer.release_date_trailer}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </section>
+          </>
+        )}
       </div>
     </section>
   );
